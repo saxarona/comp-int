154021d1bf97cd4df9694d5fdcb837430eeed548
saxarona <5231167+saxarona@users.noreply.github.com>
2022-10-11 02:09:16 +0200

    Updated with a gitignore file Safety check

--- PA04/clusterize.m
+++ PA04/clusterize.m
@@ -1,4 +1,4 @@
-## Copyright (C) Xavier Sánchez Díaz <xavier.sanchezdz@@gmail.com>
+## Copyright (C) Xavier Sánchez Díaz <mail@@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
